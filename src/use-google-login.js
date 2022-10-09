13e7f1c306da6751a4d4fed33a1c1ebae7702092
Sofiane Gargouri <contact@sofianeg.com>
2022-10-09 21:56:29 +0200

    Fixing responseType on useGoogleLogin

--- src/use-google-login.js
+++ src/use-google-login.js
@@ -90,7 +90,8 @@ const useGoogleLogin = ({
           ux_mode: uxMode,
           redirect_uri: redirectUri,
           scope,
-          access_type: accessType
+          access_type: accessType,
+          response_type: responseType
         }
 
         if (responseType === 'code') {
